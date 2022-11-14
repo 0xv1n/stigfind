65fa0c328c60101b05b40b2a2fb6d4d0fd01e83e
0xv1n <gdejes@pm.me>
2022-11-14 10:44:04 -0500

    begun work on audit policies

--- mitre-stig_win10.ps1
+++ mitre-stig_win10.ps1
@@ -36,9 +36,8 @@ Describe "Software Policies" {
     }
     # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63355.rb
     It "V-63355: Alternate operating systems must not be permitted on the same system." {
-      $setting = (bcdedit | Findstr description | Findstr /v /c:'Windows Boot Manager').Substring(24)
-      # TODO: can this be parsed/filtered better to just be 10 or Windows 10?
-      $setting | Should -Be "Windows 10"
+      $setting =  (bcdedit | Findstr description | Findstr /v /c:'Windows Boot Manager').split(" ",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Be "10"
     }
   }
 
@@ -140,6 +139,22 @@ Describe "Software Policies" {
       $setting | Should -Be 0
     }
   }
+
+  Context "Certificates" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63393.rb
+    It "V-63393: Software certificate installation files must be removed from Windows 10." {
+      # ! this is an expensive command.
+      $pfx = cmd /c 'where /R c:\ *.p12 *.pfx'
+      If ($pfx -ne '') { 
+        $setting = 1
+        break 
+      }
+      Else {
+        $setting = 0
+      }
+      $setting | Should -Be 0
+    }
+  }
 }
 
 Describe "Hardware Policies" {
@@ -231,6 +246,148 @@ Describe "Account Policies" {
     }
   }
 }
+  
+Describe "Audit Logging" {
+# ! GUIDs retrieved via -- auditpol /list /subcategory:* /r
+  Context "Account Logon" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63435.rb
+    It "V-63435: The system must be configured to audit Account Logon - Credential Validation successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE923F-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+  Context "Account Management" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63445.rb
+    It "V-63445: The system must be configured to audit Account Management - Security Group Management successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9237-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63447.rb
+    It "V-63447: The system must be configured to audit Account Management - User Account Management failures." {
+      $setting = (auditpol /get /subcategory:"{0CCE9235-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Failure", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63449.rb
+    It "V-63449: The system must be configured to audit Account Management - User Account Management successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9235-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+  
+  Context "Detailed Tracking" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63451.rb
+    It "V-63451: The system must be configured to audit Detailed Tracking - PNP Activity successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9248-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63453.rb
+    It "V-63453: The system must be configured to audit Detailed Tracking - Process Creation successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE922B-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+  Context "Logon/Logoff" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63457.rb
+    It "V-63457: The system must be configured to audit Logon/Logoff - Group Membership successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9249-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63459.rb
+    It "V-63459: The system must be configured to audit Logon/Logoff - Logoff successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9216-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63463.rb
+    It "V-63463: The system must be configured to audit Logon/Logoff - Logon failures." {
+      $setting = (auditpol /get /subcategory:"{0CCE9215-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Failure", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63467.rb
+    It "V-63467: The system must be configured to audit Logon/Logoff - Logon successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9215-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63469.rb
+    It "V-63469: The system must be configured to audit Logon/Logoff - Special Logon successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE921B-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+  Context "Object Access" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63471.rb
+    It "V-63471: The system must be configured to audit Object Access - Removable Storage failures." {
+      $setting = (auditpol /get /subcategory:"{0CCE9245-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Failure", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63473.rb
+    It "V-63473: The system must be configured to audit Object Access - Removable Storage successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9245-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+  Context "Policy Change" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63479.rb
+    It "V-63479: The system must be configured to audit Policy Change - Audit Policy Change successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE922F-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")    
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63481.rb
+    It "V-63481: The system must be configured to audit Policy Change - Authentication Policy Change successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9230-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+  Context "Privilege Use" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63483.rb
+    It "V-63483: The system must be configured to audit Privilege Use - Sensitive Privilege Use failures." {
+      $setting = (auditpol /get /subcategory:"{0CCE9228-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Failure", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63487.rb
+    It "V-63487: The system must be configured to audit Privilege Use - Sensitive Privilege Use successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9228-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+  Context "System" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63491.rb
+    It "V-63491: The system must be configured to audit System - IPSec Driver failures." {
+      $setting = (auditpol /get /subcategory:"{0CCE9213-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Failure", "Success and Failure")
+    }
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63499.rb
+    It "V-63499: The system must be configured to audit System - Other System Events successes." {
+      $setting = (auditpol /get /subcategory:"{0CCE9214-69AE-11D9-BED3-505054503030}" /r).split(",",[StringSplitOptions]'RemoveEmptyEntries')[-1]
+      $setting | Should -Not -Be "No Auditing"
+      $setting | Should -BeIn @("Success", "Success and Failure")
+    }
+  }
+
+}
 
 Describe "File Permissions" {
   Context "System Directories" {
@@ -292,6 +449,26 @@ Describe "File Permissions" {
       )
       # TODO: Needs more testing
       foreach($line in (icacls 'C:\\Windows' | Findstr /V 'Successfully' | Findstr /r /v "^$")) { $trimmed = ($line | Out-String).Trim(); If ($trimmed -in $default_perms) { $setting = 0 } Else {$setting = 1; break}  }
+      $setting | Should -Be 0
+    }
+  }
+
+  Context "File Shares" {
+    # https://github.com/mitre/microsoft-windows-10-stig-baseline/blob/master/controls/V-63357.rb
+    # TODO: Needs more testing/optimization
+    It "V-63357: Non system-created file shares on a system must limit access to groups that require it." {
+      $default_share_paths = @('C:\WINDOWS', 'C:\')
+      foreach ($path in ((Get-WMIObject -Query "SELECT * FROM Win32_Share" | Select-Object Path | Findstr /V "Path --" | Out-String).Trim()))
+      {
+        If ($path -in $default_share_paths) { continue }
+        Else {
+          If ((Get-Acl -Path $path | Format-List | Findstr /i /C:'Everyone Allow') -ne '') {
+            $setting = 1
+            break
+          }
+          Else { $setting = 0 }
+        }
+      }
       $setting | Should -Be 0
     }
   }
